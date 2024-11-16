26d68d2cd2bf1dcc2d946bbac4d65b1575d37764
TienDat <tadat290903@gmail.com>
2024-11-16 19:26:39 +0700

    update account model attribute

--- BackEnd/src/app/model/Account.js
+++ BackEnd/src/app/model/Account.js
@@ -5,6 +5,7 @@ const Account = new Schema({
     user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
     role_id: { type: Schema.Types.ObjectId, ref: "Role", required: true },
     password: {type: String},
+    is_ban: {type: Boolean, default: false},
     forgot_password_code: {type: String, maxLength: 100, default: ''},
     createdAt: {type: Date, default: Date.now},
     updatedAt: {type: Date, default: Date.now},
